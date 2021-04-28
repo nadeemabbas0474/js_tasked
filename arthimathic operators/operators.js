30aa9c8ac27a9741364dfc143e0f85277644c11a
nadeemabbas0474 <nadeemabbas0474@gmail.com>
2021-04-29 01:17:26 +0500

    ex

--- arthimathic operators/operators.js
+++ arthimathic operators/operators.js
@@ -54,18 +54,21 @@ let squ2 = num20**2;
 console.log(squ2);
 
 // increment
-num2 = ++num2;
+num2++;
 console.log(num2);
+console.log(typeof(num2));
 
-num3 = num3++;
+num3++;
 console.log(num3);
 
 // decrement
-num1 = --num1;
+num1--;
 console.log(num1);
+console.log(typeof(num1));
 
-num2 = --num2;
+num2--;
 console.log(num2);
+console.log(typeof(num2));
 
 // Augmented Addition
 
