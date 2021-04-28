4d0575ad3b6f7218f282b8e5d5da8c1a6fd2b75a
nadeemabbas0474 <nadeemabbas0474@gmail.com>
2021-04-29 00:41:49 +0500

    operation

---
+++ arthimathic operators/operators.js
@@ -0,0 +1,102 @@
+// null
+let ab , num20 ;  //decleration
+ab = null , num20 = null; //initlization
+//  numbers
+let num1 = 10 , num3 = 30 , num2 = 4;
+//  string
+let string1 = 'nadeem' , string2 = 'ali khan';
+// undefined
+let variable1 , counter;
+//  boolean
+let fls = false;
+const fourth = (10!=11);
+
+// addition operation start
+let add1 = ab + num1;
+console.log(add1);
+
+let add2 = num1 + string1;
+console.log(add2);
+
+// multiplaction start
+let multiply1 = num1 * num20;
+console.log(multiply1);
+
+let multiply2 = num3 * num1;
+console.log(multiply2);
+
+// subtraction start
+let sub1 = num3 - num1;
+console.log(sub1);
+
+let sub2 = num20 - num1;
+console.log(sub2);
+
+// dividion start
+let divid1 = num3 / num1;
+console.log(divid1);
+
+let divid2 = string1 / num1;
+console.log(divid2);
+
+// demender start
+let rem1 = num3 % num2;
+console.log(rem1);
+
+let rem2 = num3 % num1;
+console.log(rem2);
+
+// square
+let squ1 = num2**2;
+console.log(squ1);
+
+let squ2 = num20**2;
+console.log(squ2);
+
+// increment
+num2 = ++num2;
+console.log(num2);
+
+num3 = num3++;
+console.log(num3);
+
+// decrement
+num1 = --num1;
+console.log(num1);
+
+num2 = --num2;
+console.log(num2);
+
+// Augmented Addition
+
+num1 = num1+5;
+console.log(num1);
+
+num20 = num20+=10;
+console.log(num20);
+ 
+// Augmented multipication
+num3 = num3*7;
+console.log(num3);
+
+num2 = num2*=4;
+console.log(num2);
+
+// Augmented subtraction
+let num11 = 99;
+num11 = num11-21;
+console.log(num11);
+
+let num12 = 150;
+num12 = num12-=40;
+console.log(num12);
+
+// Augmented division
+let num15 = 200;
+num15 = num15/=5;
+console.log(num15);
+
+num20 = num20/=3;
+console.log(num20);
+
+
